1466d8cb699f39b24e919fb1f88af4648c1bfa54
ANSOUMANE CONDE <ansoumaneconde512@gmail.com>
2025-12-07 14:56:00 +0000

    first commit

---
+++ chaussures_homme.py
@@ -0,0 +1,36 @@
+# import the bibliothéque
+import pandas as pd
+from requests import get
+from bs4 import BeautifulSoup as bs
+
+
+#pour les chaussures-hommes
+def get_info_chaussures_hommes(containers):
+    list = []
+    for container in containers:
+        link_image = container.find('a','card-image ad__card-image waves-block waves-light').img['src']
+        price = container.find('p','ad__card-price').a.text
+        adress = container.find('p','ad__card-location').span.text
+        type = container.find('p','ad__card-description').a.text
+        dic = {
+            "type":type,
+            "adress":adress,
+            "price":price,
+            "link_image":link_image
+        }
+        list.append(dic)
+    return list
+
+
+#function for getting type chaussures-hommes
+def main_info_chaussures_hommes(size_page):
+    DF = pd.DataFrame()
+    for index in range(size_page):
+        url = f'https://sn.coinafrique.com/categorie/chaussures-homme?page={index}'
+        html_content = get(url)
+        request = bs(html_content.content,'html.parser')
+        containers = request.find_all('div','col s6 m4 l3')
+        data_list = get_info_chaussures_hommes(containers)
+        DF = pd.concat([DF,pd.DataFrame(data_list)],axis=0).reset_index(drop=True)
+    return DF
+
